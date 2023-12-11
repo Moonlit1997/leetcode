aa16003bfc124fb4449281e643865d743cf9ab1c
Moonlit1997 <zhouxiyu1997@gmail.com>
2023-12-11 11:22:50 +0800

    12.11

---
+++ 9.回文数.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=javascript
+ *
+ * [9] 回文数
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+let xString=x.toString();
+xString.
+};
+// @lc code=end
+
